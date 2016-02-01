3b4c5f013554631a2eb015e0d12e1eb1401ac8f9
Natalie <nrmartell@gmail.com>
2016-01-31 19:32:17 -0700

    Create binary.js

---
+++ binary.js
@@ -0,0 +1,15 @@
+//Implement a function that successfully adds two numbers together and returns their solution in binary. The conversion can be done before, or after the addition of the two.
+
+//The binary number returned should be a string!
+
+
+
+
+function addBinary(a,b){
+var n = a+b;
+var b = n.toString(2);
+
+return b;
+
+
+}
