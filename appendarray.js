854409bb1f1c1064dc16a563f2327832e214e344
Natalie <nrmartell@gmail.com>
2016-02-02 21:38:02 -0700

    Update appendarray.js

--- appendarray.js
+++ appendarray.js
@@ -1,3 +1,6 @@
+//Your task is to write a function named addWord that appends a string to an array. The original array should be modified and then returned
+
+
 function addWord(list, string) {
   // ...
   list.push(string);
