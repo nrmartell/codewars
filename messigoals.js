d2b8a56178290dcc6165181d35faa2f7f9e46719
Natalie <nrmartell@gmail.com>
2016-02-14 21:11:57 -0700

    Create messigoals.js

---
+++ messigoals.js
@@ -0,0 +1,19 @@
+//Messi goals function
+
+//Fill in the function goals to return the total number of goals scored by Messi using the arguments passed to the function. The arguments that are passed in are the goal tallies for la Liga, copa del rey, and champions league.
+
+//Parameters are always valid numbers between 0 and 50
+
+var laLigaGoals = 0;
+var copaDelReyGoals = 0;
+var championsLeagueGoals = 0;
+
+
+function goals (laLigaGoals, copaDelReyGoals, championsLeagueGoals) {
+
+
+
+return laLigaGoals + copaDelReyGoals + championsLeagueGoals;
+
+goals();
+}
